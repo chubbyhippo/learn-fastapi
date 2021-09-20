44ea9236e5172ac7a2d213f7d780d63dd829790b
ch <mkmsacc@outlook.com>
2021-09-20 10:53:12 +0700

    Update code

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
@@ -23,40 +24,40 @@ def is_moves_left(board: list[list[int]]):
 
 
 def evaluate(board: list[list[int]], player: int, opponent: int):
-    # Checking for Rows for X or O victory.
+    # Checking for rows if x or o is the winner
     for row in range(3):
         if board[row][0] == board[row][1] and board[row][1] == board[row][2]:
             if board[row][0] == player:
-                return 10
+                return 1
             elif board[row][0] == opponent:
-                return -10
+                return -1
 
-    # Checking for Columns for X or O victory.
+    # Checking for columns if x or o is the winner
     for col in range(3):
 
         if board[0][col] == board[1][col] and board[1][col] == board[2][col]:
 
             if board[0][col] == player:
-                return 10
+                return 1
             elif board[0][col] == opponent:
-                return -10
+                return -1
 
-    # Checking for Diagonals for X or O victory.
+    # Checking for diagonals if x or o is the winner
     if board[0][0] == board[1][1] and board[1][1] == board[2][2]:
 
         if board[0][0] == player:
-            return 10
+            return 1
         elif board[0][0] == opponent:
-            return -10
+            return -1
 
     if board[0][2] == board[1][1] and board[1][1] == board[2][0]:
 
         if board[0][2] == player:
-            return 10
+            return 1
         elif board[0][2] == opponent:
-            return -10
+            return -1
 
-    # Else if none of them have won then return 0
+    # If none of them have won then return 0
     return 0
 
 
@@ -65,22 +66,22 @@ def minimax(board: list[list[int]], depth, is_max: bool, player: int, opponent: 
 
     # If Maximizer has won
     # return evaluated score
-    if score == 10:
+    if score == 1:
         return score
 
     # If Minimizer has won
     # return evaluated score
-    if score == -10:
+    if score == -1:
         return score
 
     # If there are no more moves and no winner then
-    # It is a tie
+    # It is a draw
     if not is_moves_left(board):
         return 0
 
     # If this maximizer's move
     if is_max:
-        best = -1000
+        best = -9999
 
         # Traverse all cells
         for i in range(3):
@@ -101,7 +102,7 @@ def minimax(board: list[list[int]], depth, is_max: bool, player: int, opponent: 
 
     # If this minimizer's move
     else:
-        best = 1000
+        best = 9999
 
         # Traverse all cells
         for i in range(3):
@@ -123,12 +124,12 @@ def minimax(board: list[list[int]], depth, is_max: bool, player: int, opponent: 
 
 # This will return the best possible move for the player
 def find_best_move(board: list[list[int]], player: int, opponent: int):
-    best_val = -1000
+    best_val = -9999
     best_move = Point(-1, -1)
 
     # Traverse all cells, evaluate minimax function for
     # all empty cells. And return the cell with optimal
-    # value.
+    # value
     for i in range(3):
         for j in range(3):
 
@@ -139,7 +140,7 @@ def find_best_move(board: list[list[int]], player: int, opponent: int):
                 board[i][j] = player
 
                 # compute evaluation function for this
-                # move.
+                # move
                 move_val = minimax(board, 0, False, player, opponent)
 
                 # Undo the move
@@ -147,7 +148,7 @@ def find_best_move(board: list[list[int]], player: int, opponent: int):
 
                 # If the value of the current move is
                 # more than the best value, then update
-                # best/
+                # best
                 if move_val > best_val:
                     best_move = Point(i, j)
                     best_val = move_val
@@ -180,3 +181,7 @@ def next_turn(data_source: DataSource):
         opponent = 1
 
     return find_best_move(board, player, opponent)
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
